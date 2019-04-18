ba58f385743f3008627aecf7bea00d7c1cfa855c
TatianaChudna <tanya.papushoi@gmail.com>
2019-04-18 15:55:43 +0300

    Add home_section_8

--- final_site/wp-content/themes/my_theme/template-parts/Home/home_section_8.php
+++ final_site/wp-content/themes/my_theme/template-parts/Home/home_section_8.php
@@ -1,4 +1,31 @@
 <?php
-/* Template name: Home_section_8
+/**
+ * Template name: Home_section_8
 */
 ?>
+<?php $image = get_sub_field('image'); ?>
+
+<section class="home-section-8">
+    <div class="container">
+
+        <h1>
+            <?php the_sub_field('title'); ?>
+        </h1>
+        <p>
+            <?php the_sub_field('description'); ?>
+        </p>
+
+        <?php
+
+        if ($image): { ?>
+            <div>
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
+                     title="<?php echo $image['title'] ?> "/>
+            </div>
+            <?php
+        }
+        endif;
+        ?>
+
+    </div>
+</section>
