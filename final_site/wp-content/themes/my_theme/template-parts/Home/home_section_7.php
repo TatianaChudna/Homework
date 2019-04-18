177988aa9a87e99844bb78f2b192343e927ee448
TatianaChudna <tanya.papushoi@gmail.com>
2019-04-18 15:39:50 +0300

    Add home_section_7

--- final_site/wp-content/themes/my_theme/template-parts/Home/home_section_7.php
+++ final_site/wp-content/themes/my_theme/template-parts/Home/home_section_7.php
@@ -1,4 +1,39 @@
 <?php
-/* Template name: Home_section_7
-*/
+/**
+ * Template name: Home_section_7
+ */
 ?>
+
+<?php $link1 = get_sub_field('link1'); ?>
+<?php $link2 = get_sub_field('link2'); ?>
+<?php $image = get_sub_field('image'); ?>
+<?php $size = 'large'; ?>
+
+<section class="home-section-7">
+  <div class="container">
+
+      <?php
+
+      if ($image): { ?>
+        <div>
+          <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
+               title="<?php echo $image['title'] ?> "/>
+        </div>
+          <?php
+      }
+      endif;
+      ?>
+    <div>
+      <h1>
+          <?php the_sub_field('title'); ?>
+      </h1>
+      <p>
+          <?php the_sub_field('description'); ?>
+      </p>
+    </div>
+      <ul>
+        <li><a href="<?php echo $link1['url'] ?>"> <?php echo $link1['title'] ?></a></li>
+        <li><a href="<?php echo $link2['url'] ?>"> <?php echo $link2['title'] ?></a></li>
+      </ul>
+  </div>
+</section>
