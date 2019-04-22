5bc17894db5462bc6b2c8c358edd9caeb2a8052e
TatianaChudna <tanya.papushoi@gmail.com>
2019-04-22 19:13:50 +0300

    Add styles fot home_section_7

--- final_site/wp-content/themes/my_theme/template-parts/Home/home_section_7.php
+++ final_site/wp-content/themes/my_theme/template-parts/Home/home_section_7.php
@@ -10,29 +10,31 @@
 
 <section class="home-section-7">
   <div class="container">
+    <div class="section7-content">
 
-      <?php
+        <?php
 
-      if ($image): { ?>
-        <div>
-          <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
-               title="<?php echo $image['title'] ?> "/>
-        </div>
-          <?php
-      }
-      endif;
-      ?>
-    <div>
-      <h1>
-          <?php the_sub_field('title'); ?>
-      </h1>
-      <p>
-          <?php the_sub_field('description'); ?>
-      </p>
-    </div>
+        if ($image): { ?>
+          <div>
+            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
+                 title="<?php echo $image['title'] ?> "/>
+          </div>
+            <?php
+        }
+        endif;
+        ?>
+      <div>
+        <h1>
+            <?php the_sub_field('title'); ?>
+        </h1>
+        <p>
+            <?php the_sub_field('description'); ?>
+        </p>
+      </div>
       <ul>
-        <li><a href="<?php echo $link1['url'] ?>"> <?php echo $link1['title'] ?></a></li>
-        <li><a href="<?php echo $link2['url'] ?>"> <?php echo $link2['title'] ?></a></li>
+        <li><a class="button" href="<?php echo $link1['url'] ?>"> <?php echo $link1['title'] ?></a></li>
+        <li><a class="button" href="<?php echo $link2['url'] ?>"> <?php echo $link2['title'] ?></a></li>
       </ul>
+    </div>
   </div>
 </section>
