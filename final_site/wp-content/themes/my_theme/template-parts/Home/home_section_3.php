7f7750aeaea5455840b52ffff905227242ee2f02
TatianaChudna <tanya.papushoi@gmail.com>
2019-04-24 01:34:00 +0300

    Edit home page

--- final_site/wp-content/themes/my_theme/template-parts/Home/home_section_3.php
+++ final_site/wp-content/themes/my_theme/template-parts/Home/home_section_3.php
@@ -7,25 +7,28 @@
 
 <section class="home-section-3">
     <div class="container">
-      <div class="section3-content">
-        <h1>
-            <?php the_sub_field('title'); ?>
-        </h1>
-        <p>
-            <?php the_sub_field('description'); ?>
-        </p>
+      <div class="d-flex">
+        <div class="section3-content">
+          <h1>
+              <?php the_sub_field('title'); ?>
+          </h1>
+          <p>
+              <?php the_sub_field('description'); ?>
+          </p>
+        </div>
+
+          <?php
+
+          if ($image): { ?>
+            <div class="illustration">
+              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
+                   title="<?php echo $image['title'] ?> "/>
+            </div>
+              <?php
+          }
+          endif;
+          ?>
       </div>
 
-        <?php
-
-        if ($image): { ?>
-          <div class="illustration">
-            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
-                 title="<?php echo $image['title'] ?> "/>
-          </div>
-            <?php
-        }
-        endif;
-        ?>
     </div>
 </section>
--- final_site/wp-content/themes/my_theme/template-parts/Home/home_section_8.php
+++ final_site/wp-content/themes/my_theme/template-parts/Home/home_section_8.php
@@ -7,26 +7,27 @@
 
 <section class="home-section-8">
   <div class="container">
-    <div class="section8-content">
-      <h1>
-          <?php the_sub_field('title'); ?>
-      </h1>
+    <div class="d-flex">
+      <div class="section8-content">
+        <h1>
+            <?php the_sub_field('title'); ?>
+        </h1>
 
           <?php the_sub_field('description'); ?>
 
+      </div>
+
+        <?php
+
+        if ($image): { ?>
+          <div class="img-container">
+            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
+                 title="<?php echo $image['title'] ?> "/>
+          </div>
+            <?php
+        }
+        endif;
+        ?>
     </div>
-
-      <?php
-
-      if ($image): { ?>
-        <div class="img-container">
-          <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
-               title="<?php echo $image['title'] ?> "/>
-        </div>
-          <?php
-      }
-      endif;
-      ?>
-
   </div>
 </section>
