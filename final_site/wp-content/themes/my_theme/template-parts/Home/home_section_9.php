16a30bc981c9c9a89d31a650fa446fae13628a28
TatianaChudna <tanya.papushoi@gmail.com>
2019-04-24 01:22:04 +0300

    Add home_section_9

--- final_site/wp-content/themes/my_theme/template-parts/Home/home_section_9.php
+++ final_site/wp-content/themes/my_theme/template-parts/Home/home_section_9.php
@@ -8,24 +8,27 @@
 
 <section class="home-section-9">
   <div class="container">
-      <?php
+    <div class="d-flex">
+        <?php
 
-      if ($image): { ?>
-        <div>
-          <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
-               title="<?php echo $image['title'] ?> "/>
-        </div>
-          <?php
-      }
-      endif;
-      ?>
-    <div>
-      <h1>
-          <?php the_sub_field('title'); ?>
-      </h1>
-      <p>
-          <?php the_sub_field('description'); ?>
-      </p>
+        if ($image): { ?>
+          <div class="image-wrapper">
+            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
+                 title="<?php echo $image['title'] ?> "/>
+          </div>
+            <?php
+        }
+        endif;
+        ?>
+      <div class="section9-content">
+        <h1>
+            <?php the_sub_field('title'); ?>
+        </h1>
+        <p>
+            <?php the_sub_field('description'); ?>
+        </p>
+      </div>
     </div>
+
   </div>
 </section>
