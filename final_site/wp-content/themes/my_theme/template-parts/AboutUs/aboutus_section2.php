b9a14575f5400a8620f998cbd948713307129818
TatianaChudna <tanya.papushoi@gmail.com>
2019-04-24 09:55:46 +0300

    Add aboutus_section2

--- final_site/wp-content/themes/my_theme/template-parts/AboutUs/aboutus_section2.php
+++ final_site/wp-content/themes/my_theme/template-parts/AboutUs/aboutus_section2.php
@@ -5,30 +5,29 @@
 ?>
 
 <section class="section2">
-    <div class="container">
+  <div class="container">
+    <div class="d-flex">
         <?php $image = get_sub_field('image'); ?>
         <?php
 
         if ($image): { ?>
-            <div>
-                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
-                     title="<?php echo $image['title'] ?> "/>
-            </div>
+          <div class="image-wrapper">
+            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"
+                 title="<?php echo $image['title'] ?> "/>
+          </div>
             <?php
         }
         endif;
         ?>
 
-        <div>
-            <h1>
-                <?php the_sub_field('title'); ?>
-            </h1>
-        </div>
-        <div>
-            <p>
-                <?php the_sub_field('description'); ?>
-            </p>
-        </div>
-
+      <div class="text-block">
+        <h1>
+            <?php the_sub_field('title'); ?>
+        </h1>
+        <p>
+            <?php the_sub_field('description'); ?>
+        </p>
+      </div>
     </div>
+  </div>
 </section>
